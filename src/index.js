4301a34495fe2dfa2ddf699e3110ca866f472349
RadamantiumX <82120092+RadamantiumX@users.noreply.github.com>
2024-02-20 17:14:46 -0300

    apply new changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import messageRoutes from './routes/messages.routes.js'
 import { corsMiddleware } from './middlewares/cors.js'
+import bodyParser from 'body-parser'
 
 const app = express();
+const PORT = process.env.PORT || 3000
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.use(express.json());
 app.use(corsMiddleware());
 app.use('/api', messageRoutes)
 
-app.listen(3000)
-console.log('Server listening on port', 3000)
+app.listen(PORT)
+console.log('Server listening on port', PORT)
