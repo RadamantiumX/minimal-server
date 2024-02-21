abc4e6e0135721305df97e4ea748b79dd35a4bdb
RadamantiumX <82120092+RadamantiumX@users.noreply.github.com>
2024-02-21 16:44:49 -0300

    changes principal route

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import express from 'express';
 import messageRoutes from './routes/messages.routes.js'
 import { corsMiddleware } from './middlewares/cors.js'
-import bodyParser from 'body-parser'
+
 
 const app = express();
 const PORT = process.env.PORT || 3000
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 app.use(corsMiddleware());
-/*app.use("/", (req, res) => {
+app.use("/", (req, res) => {
     res.status(200).send({ message: "Hello There" })
-})*/
+})
 app.use('/api', messageRoutes)
 
 app.listen(PORT)
