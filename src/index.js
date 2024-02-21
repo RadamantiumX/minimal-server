d5823a24c3ce2537a500974bdade271ce3420d16
RadamantiumX <82120092+RadamantiumX@users.noreply.github.com>
2024-02-20 23:22:13 -0300

    comment code

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import bodyParser from 'body-parser'
 
 const app = express();
 const PORT = process.env.PORT || 3000
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+/*app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())*/
 app.use(express.json());
 app.use(corsMiddleware());
 /*app.use("/", (req, res) => {
