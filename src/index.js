d976e46f9cd6f2daacbf5247fdbc7834dbdfec7e
RadamantiumX <82120092+RadamantiumX@users.noreply.github.com>
2024-02-22 16:45:22 -0300

    locate app on function

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import messageRoutes from './routes/messages.routes.js'
 import { corsMiddleware } from './middlewares/cors.js'
 
 
-export const app = express();
+export const createApp = () => {
+    const app = express();
 const PORT = process.env.PORT || 3000
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
@@ -15,3 +16,5 @@ app.use('/api', messageRoutes)
 
 app.listen(PORT)
 console.log('Server listening on port', PORT)
+
+}
