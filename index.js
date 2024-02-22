fe003302c4b302724a85f26bbfe2a5d8341b2502
RadamantiumX <82120092+RadamantiumX@users.noreply.github.com>
2024-02-22 18:30:26 -0300

    change method on root

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import messageRoutes from './src/routes/messages.routes.js'
 import { corsMiddleware } from './src/middlewares/cors.js'
-
+// import { prisma } from './src/db.js'
 
 
 const app = express();
@@ -9,10 +9,20 @@ const PORT = process.env.PORT || 3000
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 app.use(corsMiddleware());
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
     res.status(200).send({ message: "Hello There" })
 })
 app.use('/api', messageRoutes)
+
+/*app.get('/messages', async (req, res) => {
+    try {
+        const messages = await prisma.message.findMany()
+       
+        res.json(messages)
+    } catch (err) {
+        next(err)
+    }
+})*/
 
 app.listen(PORT)
 console.log('Server listening on port', PORT)
