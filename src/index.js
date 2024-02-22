549ea507ef2d8f968dab6c14b9ad0f1eaa64d5f1
RadamantiumX <82120092+RadamantiumX@users.noreply.github.com>
2024-02-22 16:54:22 -0300

    more changes

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import messageRoutes from './routes/messages.routes.js'
 import { corsMiddleware } from './middlewares/cors.js'
 
 
-export const createApp = () => {
-    const app = express();
+
+const app = express();
 const PORT = process.env.PORT || 3000
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
@@ -16,5 +16,3 @@ app.use('/api', messageRoutes)
 
 app.listen(PORT)
 console.log('Server listening on port', PORT)
-
-}
