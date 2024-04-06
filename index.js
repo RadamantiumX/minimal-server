a1aa21fc2918d177901f164b1277c55618870863
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-04-05 22:14:52 -0300

    functions deploy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import messageRoutes from './src/routes/messages.routes.js'
 import { corsMiddleware } from './src/middlewares/cors.js'
 import bodyParser from 'body-parser'
 
-
+export default function handler (){
 const app = express();
 const PORT = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -12,7 +12,7 @@ app.use(corsMiddleware);
 
 // Muy importante: NO UTILIZAR MEOTODO "use()" EN RUTA RAIZ
 app.get("/", (req, res) => {
-    res.status(200).json({ message: "Hello There" })
+    res.status(200).json({ message: "Server on" })
 })
 //////////////////////
 
@@ -22,4 +22,7 @@ app.use('/api', messageRoutes)
 
 app.listen(PORT)
 console.log(`Server started on http://localhost:${PORT}`)
+}
+
+
 
