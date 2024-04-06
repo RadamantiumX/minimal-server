0ccc1fa3693859638b78a3243c3071b461a782a7
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-04-05 23:04:30 -0300

    delete a origin

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -7,7 +7,6 @@ const ACCEPTED_ORIGINS = [
     'http://localhost:3002',
     'http://localhost:3003',
     'http://localhost:3004',
-    'http://localhost:3005',
     'http://localhost:4000',
     'http://localhost:5173',
     'https://epikka-new.vercel.app'
