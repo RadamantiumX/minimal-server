19c37dfa8ad783f7297903b42da6d0460c4f4be1
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-05-17 16:46:12 -0300

    another test

--- src/routes/pornhub.routes.js
+++ src/routes/pornhub.routes.js
@@ -8,7 +8,7 @@ pornhubRouter.get('/models', async (req, res) => {
 
    try{
         const pornhub = new PornHub()
-        const models = await pornhub.pornstar('https://www.pornhub.com/pornstar/eva-elfie')
+        const models = await pornhub.webMaster.getPornstars()
 
         res.status(200).json({ models: models })
     } catch (err) {
